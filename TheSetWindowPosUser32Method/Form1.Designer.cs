12b7a87749f5b0a0873f4499394af2c383918083
Kulpot <kulpotwahu@gmail.com>
2023-06-01 06:05:13 +0800

    Add project files.

---
+++ TheSetWindowPosUser32Method/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TheSetWindowPosUser32Method
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExpand = new System.Windows.Forms.Button();
+            this.btnPosition = new System.Windows.Forms.Button();
+            this.btnTop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExpand
+            // 
+            this.btnExpand.Location = new System.Drawing.Point(12, 12);
+            this.btnExpand.Name = "btnExpand";
+            this.btnExpand.Size = new System.Drawing.Size(152, 35);
+            this.btnExpand.TabIndex = 0;
+            this.btnExpand.Text = "Expand";
+            this.btnExpand.UseVisualStyleBackColor = true;
+            this.btnExpand.Click += new System.EventHandler(this.btnExpand_Click);
+            // 
+            // btnPosition
+            // 
+            this.btnPosition.Location = new System.Drawing.Point(12, 53);
+            this.btnPosition.Name = "btnPosition";
+            this.btnPosition.Size = new System.Drawing.Size(152, 35);
+            this.btnPosition.TabIndex = 1;
+            this.btnPosition.Text = "Position";
+            this.btnPosition.UseVisualStyleBackColor = true;
+            this.btnPosition.Click += new System.EventHandler(this.btnPosition_Click);
+            // 
+            // btnTop
+            // 
+            this.btnTop.Location = new System.Drawing.Point(12, 94);
+            this.btnTop.Name = "btnTop";
+            this.btnTop.Size = new System.Drawing.Size(152, 35);
+            this.btnTop.TabIndex = 2;
+            this.btnTop.Text = "Set TopMost";
+            this.btnTop.UseVisualStyleBackColor = true;
+            this.btnTop.Click += new System.EventHandler(this.btnTop_Click);
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 255);
+            this.Controls.Add(this.btnTop);
+            this.Controls.Add(this.btnPosition);
+            this.Controls.Add(this.btnExpand);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExpand;
+        private System.Windows.Forms.Button btnPosition;
+        private System.Windows.Forms.Button btnTop;
+    }
+}
+
---
+++ TheSetWindowPosUser32Method/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// ref link:https://www.youtube.com/watch?v=XiwzFJBNeKA&list=PLAIBPfq19p2EJ6JY0f5DyQfybwBGVglcR&index=74&t=325s
+// READ Link: MSDN SetWindowPos function //Copy(Signature/ User-Defined Types): Link: pinvoke.net: SetWindowPos
+
+namespace TheSetWindowPosUser32Method
+{
+    public partial class form1 : Form
+    {
+        #region Native
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        // Returns zero if function fails, otherwise return a non-zero value
+
+        // effected window      z-order indication    xPos   yPos   width    height   flags         
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+
+        // Flags
+        const int SWP_ASYNCWINDOWPOS = 0x4000;
+        const int SWP_DEFERERASE = 0x2000;
+        const int SWP_DRAWFRAME = 0x0020;
+        const int SWP_FRAMECHANGED = 0x0020;
+        const int SWP_HIDEWINDOW = 0x0080;
+        const int SWP_NOACTIVATE = 0x0010;
+        const int SWP_NOCOPYBITS = 0x0100;
+        const int SWP_NOMOVE = 0x0002;
+        const int SWP_NOOWNERZORDER = 0x0200;
+        const int SWP_NOREDRAW = 0x0008;
+        const int SWP_NOREPOSITION = 0x0200;
+        const int SWP_NOSENDCHANGING = 0x0400;
+        const int SWP_NOSIZE = 0x0001;
+        const int SWP_NOZORDER = 0x0004;
+        const int SWP_SHOWWINDOW = 0x0040;
+
+        const int HWND_TOP = 0; // Bring to front
+        const int HWND_BOTTOM = 1; // Send to back
+        const int HWND_TOPMOST = -1; // Bring to front and stay on top of other windows that do not have this flag set
+        const int HWND_NOTOPMOST = -2; // Disable topmost for the window
+        #endregion
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExpand_Click(object sender, EventArgs e)
+        {
+            bool hasSmallerWidth = true;
+            bool hasSmallerHeight = true;
+
+            while(hasSmallerWidth || hasSmallerHeight)
+            {
+                hasSmallerWidth = this.Size.Width < Screen.PrimaryScreen.WorkingArea.Width;
+                hasSmallerHeight = this.Size.Height < Screen.PrimaryScreen.WorkingArea.Height;
+
+                if (hasSmallerWidth)
+                {
+                    //this.Width++;
+                    SetWindowPos(this.Handle, IntPtr.Zero, 0, 0, this.Width + 1, this.Height, SWP_ASYNCWINDOWPOS);
+                }
+
+                if (hasSmallerHeight)
+                {
+                    //this.Height++;
+                    SetWindowPos(this.Handle, IntPtr.Zero, 0, 0, this.Width, this.Height + 1, SWP_ASYNCWINDOWPOS);
+                }
+            }
+        }
+
+        private void btnPosition_Click(object sender, EventArgs e)
+        {
+            SetWindowPos(this.Handle, (IntPtr)HWND_TOP, 100, 100, 45454545, 4544545, SWP_NOSIZE);
+        }
+
+        private void btnTop_Click(object sender, EventArgs e)
+        {
+            SetWindowPos(this.Handle, (IntPtr)HWND_TOPMOST, 45545, 45545, 454545, 45545, SWP_NOSIZE | SWP_NOMOVE);
+        }
+    }
+}
